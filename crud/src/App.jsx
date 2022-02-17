1c777ee930185beeff991a363341c40cd31481b8
Nancy <nancy.090495@gmail.com>
2022-02-16 19:35:20 -0600

    add the delete in homeworks

---
+++ crud/src/App.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { nanoid } from "nanoid";
+//setTareas([...tareas, { id: nanoid(), NombreTarea: tarea }]);
+function App() {
+  const [homework, setHomework] = React.useState("");
+  const [homeworks, setHomeworks] = React.useState([]);
+
+  const addHomework = (e) => {
+    e.preventDefault();
+    if (!homework.trim()) {
+      console.log("Empty element");
+      return;
+    }
+    console.log(homework);
+
+    setHomeworks([
+      ...homeworks,
+      {
+        id: nanoid(),
+        nameHomework: homework,
+      },
+    ]);
+    setHomework("");
+  };
+
+  const deleteHomework = (id) => {
+    //console.log(id);
+    const arrayFilter = homeworks.filter((item) => item.id !== id);
+    setHomeworks(arrayFilter);
+  };
+  return (
+    <div className="container mt-5">
+      <h1 className="text-center">CRUD simple</h1>
+      <hr />
+      <div className="row">
+        <div className="col-8">
+          <h4 className="text-center">Homework List</h4>
+          <ul className="list-group">
+            {homeworks.map((item) => (
+              <li className="list-group-item" key={item.id}>
+                <span className="lead">{item.nameHomework}</span>
+                <button
+                  className="btn btn-danger btn-sm float-end mx-2"
+                  onClick={() => deleteHomework(item.id)}
+                >
+                  Delete
+                </button>
+                <button className="btn btn-warning btn-sm float-end">
+                  Edit
+                </button>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="col-4">
+          <h4 className="text-center">Form</h4>
+          <form onSubmit={addHomework}>
+            <input
+              type="text"
+              className="form-control mb-2"
+              placeholder="Enter homework"
+              onChange={(e) => setHomework(e.target.value)}
+              value={homework}
+            />
+            <div className="d-grid">
+              <button className="btn btn-dark" type="submit">
+                Add
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
