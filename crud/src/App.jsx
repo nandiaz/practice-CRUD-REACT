ce55bf3951810cbe42e0d536d71958062604e50d
Nancy <nancy.090495@gmail.com>
2022-02-21 12:20:37 -0600

    add reoutes

--- crud/src/App.jsx
+++ crud/src/App.jsx
@@ -1,32 +1,24 @@
 import React from "react";
-import Coments from "./components/Coments";
-import Greet from "./components/Greet";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>Proyecto desde 0</h1>
-      <Greet people="Nancy" age={26} />
-      <Greet people="Arturo" age={30} />
-      <Greet people="Alejandro" age={33} />
-      <hr />
-      <h3>Box coments</h3>
-      <Coments
-        urlImage="https://picsum.photos/64"
-        subject="Nancy"
-        text="hola mi nombre es Nancy"
-      />
-      <Coments
-        urlImage="https://picsum.photos/64"
-        subject="Arturo"
-        text="hola mi nombre es Arturo"
-      />
-      <Coments
-        urlImage="https://picsum.photos/64"
-        subject="Alejandro"
-        text="hola mi nombre es Alejandro"
-      />
-    </div>
+    <Router>
+      <div className="container">
+        <h1>All ok...</h1>
+        <hr />
+        <Routes>
+          <Route path="/">This is the homepage </Route>
+          <Route path="/contact">This is the contact page</Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
